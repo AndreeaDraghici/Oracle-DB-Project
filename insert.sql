4fc188625ba480cc3b0a0bfbacff0519bd1d1382
Drăghici Andreea-Maria <draghici108@gmail.com>
2023-01-04 11:54:46 +0200

    Add files via upload

---
+++ insert.sql
@@ -0,0 +1,1557 @@
+INSERT INTO Nava VALUES (
+	250,		
+	'Ama',
+    '158 pasageri',
+    2017,	
+	'Stationeaza'	
+);
+
+INSERT INTO Nava VALUES (
+	252,		
+	'Titanic',
+    '2908 pasageri',
+    2023,		
+	'Navigheaza'	
+);
+
+INSERT INTO Nava VALUES (
+	251,		
+    'Azamara',
+    '676 pasageri',
+    2007,
+	'Stationeaza'	
+);
+
+
+INSERT INTO Nava VALUES (
+	253,		
+	'Celestyal',
+	'1800 pasageri',
+	1993,
+	'Navigheaza'	
+);
+
+INSERT INTO Nava VALUES (
+	254,		
+	'Disney',
+	'2500 pasageri',
+	2011,		
+	'Navigheaza'	
+);
+
+INSERT INTO Nava VALUES (
+	255,		
+	'Holland',
+	'2600 pasageri',
+	2018,	
+	'Stationeaza'	
+);
+
+INSERT INTO Nava VALUES (
+	256,		
+	'Luftner',
+	'170 pasageri',
+	2021,	
+	'Navigheaza'	
+);
+
+INSERT INTO Nava VALUES (
+	257,		
+	'MSC',
+	'6850 pasageri',
+	2022,	
+	'Navigheaza'	
+);
+
+INSERT INTO Nava VALUES (
+	258,		
+	'Norwegian',
+    '3950 pasageri',
+     2022,		
+	'Navigheaza'	
+);
+
+INSERT INTO Nava VALUES (
+	259,		
+	'Oceania',
+	'688 pasageri',
+	1999,
+	'Navigheaza'	
+);
+
+INSERT INTO Nava VALUES (
+	260,		
+	'Ponant',
+	'332 pasageri',
+	1998,		
+	'Navigheaza'	
+);
+
+INSERT INTO Nava VALUES (
+	261,		
+	'Regent',
+	'750 pasageri',
+	2016,		
+	'Stationeaza'	
+);
+
+INSERT INTO Nava VALUES (
+	262,		
+	'Royal',
+	'6296 pasageri',
+	2021,		
+	'Navigheaza'	
+);
+
+INSERT INTO Nava VALUES (
+	263,		
+	'Seabourn',
+	'604 pasageri',
+	2016,		
+	'Navigheaza'	
+);
+
+INSERT INTO Nava VALUES (
+	264,		
+	'Silversea',
+	'596 pasageri',
+	2017,	
+	'Navigheaza'	
+);
+
+INSERT INTO Nava VALUES (
+	265,		
+	'Viking',
+	'930 pasageri',
+	2014,		
+	'Navigheaza'	
+);
+
+INSERT INTO Nava VALUES (
+	266,		
+	'Windstar',
+	'312 pasageri',
+	1990,	
+	'Navigheaza'	
+);
+
+INSERT INTO Echipaj VALUES (
+	115,
+	250,
+	'Grave',
+	50	
+);
+
+INSERT INTO Echipaj VALUES (
+	116,
+	251,	
+	'Chantie',
+	407	
+);
+
+INSERT INTO Echipaj VALUES (
+	117,
+	252,		
+	'Meyer',
+	1120	
+);
+
+INSERT INTO Echipaj VALUES (
+	118,
+	253,		
+	'Fincantieri',
+	662
+);
+
+INSERT INTO Echipaj VALUES (
+	119,
+	254,	
+	'Werft',
+	1458
+);
+
+INSERT INTO Echipaj VALUES (
+	120,
+	255,	
+	'Marghera',
+	1025
+);
+
+INSERT INTO Echipaj VALUES (
+	121,
+	256,	
+	'Shipyard',
+	46
+);
+
+INSERT INTO Echipaj VALUES (
+	122,
+	257,	
+	'Nazaire',
+	2138
+);
+
+INSERT INTO Echipaj VALUES (
+	123,
+	258,		
+	'Winder',
+	1388
+);
+
+INSERT INTO Echipaj VALUES (
+	124,
+	259,		
+	'Valer',
+	373
+);
+
+INSERT INTO Echipaj VALUES (
+	125,
+	260,		
+	'Atlant',
+	50
+);
+
+INSERT INTO Echipaj VALUES (
+	126,
+	261,	
+	'Fantastik',
+	542
+);
+
+INSERT INTO Echipaj VALUES (
+	127,
+	262,	
+	'Cartier',
+	2300
+);
+
+INSERT INTO Echipaj VALUES (
+	128,
+	263,		
+	'Mariotti',
+	450
+);
+
+INSERT INTO Echipaj VALUES (
+	129,
+	264,		
+	'Erin',
+	411
+);
+
+INSERT INTO Echipaj VALUES (
+	130,
+	265,		
+	'Yard',
+	465
+);
+
+INSERT INTO Echipaj VALUES (
+	131,
+	266,		
+	'Havre',
+	163
+);
+
+INSERT INTO Persoana VALUES (
+	1870517101215,
+	'Aciu Mircea',
+	DATE '1987-01-01'
+);
+
+INSERT INTO Persoana VALUES (
+	2870517101216,
+	'Aciu Lia',
+	DATE '1987-02-01'
+);
+
+INSERT INTO Persoana VALUES (
+	1870517101217,
+	'Cojocaru Cornel',
+	DATE '1988-02-01'
+);
+
+INSERT INTO Persoana VALUES (
+	2870517101218,
+	'Badea Ana',
+	DATE '1986-03-02'
+);
+
+INSERT INTO Persoana VALUES (
+	1870517101219,
+	'Braga Nicolae',
+	DATE '1982-04-05'
+);
+
+INSERT INTO Persoana VALUES (
+	2870517101220,
+	'Badescu Lavinia',
+	DATE '1984-04-04'
+);
+
+INSERT INTO Persoana VALUES (
+	1870517101221,
+	'Badic Mihai',
+	DATE '1988-04-05'
+);
+
+INSERT INTO Persoana VALUES (
+	2870517101222,
+	'Nicolae Ana',
+	DATE '1987-05-10'
+);
+
+INSERT INTO Persoana VALUES (
+	1870517101223,
+	'Budescu Octavian',
+	DATE '1982-06-15'
+);
+
+INSERT INTO Persoana VALUES (
+	2870517101224,
+	'Baran Maria',
+	DATE '1989-07-21'
+);
+
+INSERT INTO Persoana VALUES (
+	1870517101225,
+	'Bidana Paul',
+	DATE '1978-08-25'
+);
+
+INSERT INTO Persoana VALUES (
+	2870517101226,
+	'Bitina Elena',
+	DATE '1978-09-13'
+);
+
+INSERT INTO Persoana VALUES (
+	1870517101227,
+	'Cernat Eugen',
+	DATE '1978-10-17'
+);
+
+INSERT INTO Persoana VALUES (
+	2870517101228,
+	'Costea Mariana',
+	DATE '1979-11-11'
+);
+
+INSERT INTO Persoana VALUES (
+	1870517101229,
+	'Darie Sorin',
+	DATE '1975-12-20'
+);
+
+INSERT INTO Persoana VALUES (
+	2870517101230,
+	'Calin Maria',
+	DATE '1975-12-27'
+);
+
+INSERT INTO Persoana VALUES (
+	1870517101231,
+	'David Valeriu',
+	DATE '1974-10-01'
+);
+
+INSERT INTO Persoana VALUES (
+	2870517101232,
+	'Coca Mihaela',
+	DATE '1978-02-23'
+);
+
+INSERT INTO Persoana VALUES (
+	1870517101233,
+	'Dobre Ion',
+	DATE '1975-12-03'
+);
+
+INSERT INTO Persoana VALUES (
+	2870517101234,
+	'Duma Denisa',
+	DATE '1989-10-03'
+);
+
+INSERT INTO Persoana VALUES (
+	1870517101235,
+	'Dumbrava Ionel',
+	DATE '1982-11-30'
+);
+
+INSERT INTO Persoana VALUES (
+	2870517101236,
+	'Gliga Ioana',
+	DATE '1987-12-12'
+);
+
+INSERT INTO Persoana VALUES (
+	1870517101237,
+	'Goiceanu Cristian',
+	DATE '1988-10-01'
+);
+
+INSERT INTO Persoana VALUES (
+	2870517101238,
+	'Constantinescu Claudia',
+	DATE '1989-03-02'
+);
+
+INSERT INTO Persoana VALUES (
+	1870517101239,
+	'Heler Mihai',
+	DATE '1973-03-22'
+);
+
+INSERT INTO Persoana VALUES (
+	2870517101240,
+	'Lazar Mihaela',
+	DATE '1981-04-25'
+);
+
+INSERT INTO Persoana VALUES (
+	1870517101241,
+	'Lazar Dan',
+	DATE '1981-04-05'
+);
+
+INSERT INTO Persoana VALUES (
+	2870517101242,
+	'Letiu Laura',
+	DATE '1986-05-09'
+);
+
+INSERT INTO Persoana VALUES (
+	1870517101243,
+	'Lunca Eduard',
+	DATE '1988-06-07'
+);
+
+INSERT INTO Persoana VALUES (
+	2870517101244,
+	'Marinescu Andreea',
+	DATE '1986-07-17'
+);
+
+INSERT INTO Persoana VALUES (
+	1870517101245,
+	'Marin Claudiu',
+	DATE '1973-08-05'
+);
+
+INSERT INTO Persoana VALUES (
+	2870517101246,
+	'Militaru Stefania',
+	DATE '1989-09-21'
+);
+
+INSERT INTO Persoana VALUES (
+	1870517101247,
+	'Mihai Catalin',
+	DATE '1989-10-14'
+);
+
+INSERT INTO Persoana VALUES (
+	2870517101248,
+	'Manea Mihaela',
+	DATE '1985-11-27'
+);
+
+INSERT INTO Persoana VALUES (
+	1870517101249,
+	'Mosoiu Adrian',
+	DATE '1978-12-30'
+);
+
+INSERT INTO Persoana VALUES (
+	2870517101250,
+	'Munteanu Catalina',
+	DATE '1974-01-09'
+);
+
+INSERT INTO Persoana VALUES (
+	1870517101251,
+	'Neamtu Liviu',
+	DATE '1980-02-20'
+);
+
+INSERT INTO Persoana VALUES (
+	2870517101252,
+	'Oanta Octavia',
+	DATE '1987-03-23'
+);
+
+INSERT INTO Persoana VALUES (
+	1870517101253,
+	'Paunescu George',
+	DATE '1971-04-28'
+);
+
+INSERT INTO Persoana VALUES (
+	2870517101254,
+	'Patru Ioana',
+	DATE '1986-05-16'
+);
+
+INSERT INTO Persoana VALUES (
+	1870517101255,
+	'Popa Adrian',
+	DATE '1976-06-12'
+);
+
+INSERT INTO Persoana VALUES (
+	2870517101256,
+	'Popescu Ana',
+	DATE '1974-07-20'
+);
+
+INSERT INTO Persoana VALUES (
+	1870517101257,
+	'Popov Dragos',
+	DATE '1980-08-19'
+);
+
+INSERT INTO Persoana VALUES (
+	2870517101258,
+	'Radu Nicolae',
+	DATE '1973-09-07'
+);
+
+INSERT INTO Persoana VALUES (
+	1870517101259,
+	'Raducan Andrada',
+	DATE '1982-10-18'
+);
+
+INSERT INTO Persoana VALUES (
+	2870517101260,
+	'Radoi Mirel',
+	DATE '1980-11-21'
+);
+
+INSERT INTO Persoana VALUES (
+	1870517101261,
+	'Sandu Florin',
+	DATE '1974-12-06'
+);
+
+INSERT INTO Persoana VALUES (
+	2870517101262,
+	'Sanda Adelina',
+	DATE '1986-01-08'
+);
+
+INSERT INTO Persoana VALUES (
+	1870517101263,
+	'Simion Marius',
+	DATE '1980-02-02'
+);
+
+INSERT INTO Persoana VALUES (
+	2870517101264,
+	'Silaghi Marina',
+	DATE '1987-03-16'
+);
+
+INSERT INTO Persoana VALUES (
+	1870517101265,
+	'Stefan Andrei',
+	DATE '1977-04-24'
+);
+
+INSERT INTO Persoana VALUES (
+	2870517101266,
+	'Tataru Ana',
+	DATE '1988-05-04'
+);
+
+INSERT INTO Persoana VALUES (
+	1870517101267,
+	'Tenea Catalin',
+	DATE '1989-06-27'
+);
+
+INSERT INTO Persoana VALUES (
+	2870517101268,
+	'Voicu Maria',
+	DATE '1982-07-18'
+);
+
+INSERT INTO Persoana VALUES (
+	1870517101269,
+	'Vintila Anghel',
+	DATE '1982-08-08'
+);
+
+INSERT INTO Membrii VALUES (
+	1320,
+	1870517101215,
+	115,
+	'Personal de punte'
+);
+
+INSERT INTO Membrii VALUES (
+	1321,
+	28705171012116,
+	116,
+	'Personal de masini'
+);
+
+INSERT INTO Membrii VALUES (
+	1322,
+	1870517101217,
+	117,
+	'Personal de radiocomunicatii'
+);
+
+INSERT INTO Membrii VALUES (
+	1323,
+	2870517101218,
+	118,
+	'Personal de auxiliar'
+);
+
+INSERT INTO Membrii VALUES (
+	1324,
+	1870517101219,
+	119,
+	'Personal de punte'
+);
+
+INSERT INTO Membrii VALUES (
+	1325,
+	2870517101220,
+	120,
+	'Personal de masini'
+);
+
+INSERT INTO Membrii VALUES (
+	1326,
+	1870517101221,
+	121,
+	'Personal de radiocomunicatii'
+);
+
+INSERT INTO Membrii VALUES (
+	1327,
+	2870517101222,
+	122,
+	'Personal de auxiliar'
+);
+
+INSERT INTO Membrii VALUES (
+	1328,
+	1870517101223,
+	123,
+	'Personal de punte'
+);
+
+INSERT INTO Membrii VALUES (
+	1329,
+	2870517101224,
+	124,
+	'Personal de masini'
+);
+
+INSERT INTO Membrii VALUES (
+	1330,
+	1870517101225,
+	125,
+	'Personal de radiocomunicatii'
+);
+
+INSERT INTO Membrii VALUES (
+	1331,
+	2870517101226,
+	126,
+	'Personal de auxiliar'
+);
+
+INSERT INTO Membrii VALUES (
+	1332,
+	1870517101227,
+	127,
+	'Personal de punte'
+);
+
+INSERT INTO Membrii VALUES (
+	1333,
+	2870517101228,
+	128,
+	'Personal de punte'
+);
+
+INSERT INTO Membrii VALUES (
+	1334,
+	1870517101229,
+	129,
+	'Personal de masini'
+);
+
+INSERT INTO Membrii VALUES (
+	1335,
+	2870517101230,
+	130,
+	'Personal de radiocomunicatii'
+);
+
+INSERT INTO Membrii VALUES (
+	1336,
+	1870517101231,
+	131,
+	'Personal de auxiliar'
+);
+
+INSERT INTO Membrii VALUES (
+	1337,
+	2870517101232,
+	132,
+	'Personal de punte'
+);
+
+INSERT INTO Membrii VALUES (
+	1338,
+	1870517101233,
+	117,
+	'Personal de masini'
+);
+
+INSERT INTO Membrii VALUES (
+	1339,
+	2870517101234,
+	122,
+	'Personal de radiocomunicatii'
+);
+
+INSERT INTO Membrii VALUES (
+	1340,
+	1870517101235,
+	123,
+	'Personal de auxiliar'
+);
+
+INSERT INTO Membrii VALUES (
+	1341,
+	2870517101236,
+	127,
+	'Personal de punte'
+);
+
+INSERT INTO Membrii VALUES (
+	1342,
+	1870517101237,
+	117,
+	'Personal de masini'
+);
+
+INSERT INTO Membrii VALUES (
+	1343,
+	2870517101238,
+	122,
+	'Personal de radiocomunicatii'
+);
+
+INSERT INTO Membrii VALUES (
+	1344,
+	1870517101239,
+	123,
+	'Personal de auxiliar'
+);
+
+INSERT INTO Membrii VALUES (
+	1345,
+	2870517101240,
+	127,
+	'Personal de punte'
+);
+
+INSERT INTO Pasager VALUES (
+	2140,
+	2870517101240,
+	10
+);
+
+INSERT INTO Pasager VALUES (
+	2141,
+	1870517101241,
+	10
+);
+
+INSERT INTO Pasager VALUES (
+	2142,
+	2870517101242,
+	10
+);
+INSERT INTO Pasager VALUES (
+	2143,
+	1870517101243,
+	10
+);
+
+INSERT INTO Pasager VALUES (
+	2144,
+	2870517101244,
+	11
+);
+
+INSERT INTO Pasager VALUES (
+	2145,
+	1870517101245,
+	11
+);
+
+INSERT INTO Pasager VALUES (
+	2146,
+	2870517101246,
+	11
+);
+
+INSERT INTO Pasager VALUES (
+	2147,
+	1870517101247,
+	11
+);
+
+INSERT INTO Pasager VALUES (
+	2148,
+	2870517101248,
+	12
+);
+
+INSERT INTO Pasager VALUES (
+	2149,
+	1870517101249,
+	13
+);
+
+INSERT INTO Pasager VALUES (
+	2150,
+	2870517101250,
+	13
+);
+
+INSERT INTO Pasager VALUES (
+	2151,
+	1870517101251,
+	13
+);
+
+INSERT INTO Pasager VALUES (
+	2152,
+	2870517101252,
+	13
+);
+
+INSERT INTO Pasager VALUES (
+	2153,
+	1870517101253,
+	14
+);
+
+INSERT INTO Pasager VALUES (
+	2154,
+	2870517101254,
+	14
+);
+
+INSERT INTO Pasager VALUES (
+	2155,
+	1870517101255,
+	14
+);
+
+INSERT INTO Pasager VALUES (
+	2156,
+	2870517101256,
+	14
+);
+
+INSERT INTO Pasager VALUES (
+	2157,
+	1870517101257,
+	15
+);
+
+INSERT INTO Pasager VALUES (
+	2158,
+	2870517101258,
+	16
+);
+
+INSERT INTO Pasager VALUES (
+	2159,
+	1870517101259,
+	17
+);
+
+INSERT INTO Pasager VALUES (
+	2160,
+	2870517101260,
+	18
+);
+
+INSERT INTO Pasager VALUES (
+	2161,
+	1870517101261,
+	19
+);
+
+INSERT INTO Pasager VALUES (
+	2162,
+	2870517101262,
+	20
+);
+
+INSERT INTO Pasager VALUES (
+	2163,
+	1870517101263,
+	21
+);
+
+INSERT INTO Pasager VALUES (
+	2164,
+	2870517101264,
+	22
+);
+
+INSERT INTO Pasager VALUES (
+	2165,
+	1870517101265,
+	23
+);
+
+INSERT INTO Pasager VALUES (
+	2166,
+	2870517101266,
+	24
+);
+
+INSERT INTO Pasager VALUES (
+	2167,
+	1870517101267,
+	25
+);
+
+INSERT INTO Pasager VALUES (
+	2168,
+	2870517101268,
+	26
+);
+
+INSERT INTO Pasager VALUES (
+	2169,
+	1870517101269,
+	27
+);
+
+
+INSERT INTO Croaziera VALUES (
+	610,
+	'Aidanova',
+	'Insulele Canare',
+	'7 nopti',
+	'De la 499 EUR'
+);
+
+INSERT INTO Croaziera VALUES (
+	611,
+	'Aicosma',
+	'Barcelona',
+	'8 nopti',
+	'De la 687 EUR'
+);
+
+INSERT INTO Croaziera VALUES (
+	612,
+	'Equinox',
+	'Fort Lauderdale',
+	'6 nopti',
+	'De la 662 EUR'
+);
+
+INSERT INTO Croaziera VALUES (
+	613,
+	'Millenium',
+	'Tampa',
+	'10 nopti',
+	'De la 989 EUR'
+);
+
+INSERT INTO Croaziera VALUES (
+	614,
+	'Olympia',
+	'Piraeus',
+	'3 nopti',
+	'De la 379 EUR'
+);
+
+INSERT INTO Croaziera VALUES (
+	615,
+	'Lavri',
+	'Athena',
+	'4 nopti',
+	'De la 489 EUR'
+);
+
+INSERT INTO Croaziera VALUES (
+	616,
+	'Victoria',
+	'Southampton',
+	'7 nopti',
+	'De la 1080 EUR'
+);
+
+INSERT INTO Croaziera VALUES (
+	617,
+	'Elizabeth',
+	'Mediterana V',
+	'7 nopti',
+	'De la 1200 EUR'
+);
+
+INSERT INTO Croaziera VALUES (
+	618,
+	'Westerdam',
+	'Alaska',
+	'7 nopti',
+	'De la 503 EUR'
+);
+
+INSERT INTO Croaziera VALUES (
+	619,
+	'Eurodam',
+	'Vancouver',
+	'7 nopti',
+	'De la 521 EUR'
+);
+
+INSERT INTO Croaziera VALUES (
+	620,
+	'Harald',
+	'Scandinavia',
+	'5 nopti',
+	'De la 887 EUR'
+);
+
+INSERT INTO Croaziera VALUES (
+	621,
+	'Richard',
+	'Kirkenes',
+	'5 nopti',
+	'De la 991 EUR'
+);
+
+INSERT INTO Croaziera VALUES (
+	622,
+	'Meraviglia',
+	'Bahamas',
+	'3 nopti',
+	'De la 119 EUR'
+);
+
+INSERT INTO Croaziera VALUES (
+	623,
+	'Sinfonia',
+	'Venetia',
+	'4 nopti',
+	'De la 199 EUR'
+);
+
+INSERT INTO Croaziera VALUES (
+	624,
+	'Opera',
+	'Napoli',
+	'7 nopti',
+	'De la 299 EUR'
+);
+
+INSERT INTO Croaziera VALUES (
+	625,
+	'Magnifica',
+	'Hamburg',
+	'11 nopti',
+	'De la 329 EUR'
+);
+
+INSERT INTO Croaziera VALUES (
+	626,
+	'Poesia',
+	'Barcelona',
+	'10 nopti',
+	'De la 479 EUR'
+);
+
+INSERT INTO Croaziera VALUES (
+	627,
+	'Divina',
+	'Miami',
+	'10 nopti',
+	'De la 539 EUR'
+);
+
+INSERT INTO Croaziera VALUES (
+	628,
+	'Cruise',
+	'Mexic',
+	'5 nopti',
+	'De la 426 EUR'
+);
+
+INSERT INTO Croaziera VALUES (
+	629,
+	'Epic',
+	'San Juan',
+	'6 nopti',
+	'De la 499 EUR'
+);
+
+INSERT INTO Croaziera VALUES (
+	630,
+	'Riviera',
+	'Lisabona',
+	'7 nopti',
+	'De la 1479 EUR'
+);
+
+INSERT INTO Croaziera VALUES (
+	631,
+	'Marina',
+	'Marea Baltica',
+	'7 nopti',
+	'De la 1799 EUR'
+);
+
+INSERT INTO Croaziera VALUES (
+	632,
+	'Regatta',
+	'Polinezia Franceza',
+	'10 nopti',
+	'De la 2429 EUR'
+);
+
+INSERT INTO Croaziera VALUES (
+	633,
+	'Nautica',
+	'Valletta',
+	'10 nopti',
+	'De la 2469 EUR'
+);
+
+INSERT INTO Croaziera VALUES (
+	634,
+	'Anthem',
+	'Fiordurile Norvegiei',
+	'7 nopti',
+	'De la 788 EUR'
+);
+
+INSERT INTO Croaziera VALUES (
+	635,
+	'Rhapsody',
+	'Caraibe de Sud',
+	'14 nopti',
+	'De la 1033 EUR'
+);
+
+INSERT INTO Croaziera VALUES (
+	636,
+	'Princess',
+	'Melbourne',
+	'5 nopti',
+	'De la 490 EUR'
+);
+
+INSERT INTO Croaziera VALUES (
+	637,
+	'Diamond',
+	'Japonia',
+	'5 nopti',
+	'De la 645 EUR'
+);
+
+INSERT INTO Croaziera VALUES (
+	638,
+	'Carib',
+	'San Francisco',
+	'18 nopti',
+	'De la 972 EUR'
+);
+
+INSERT INTO Croaziera VALUES (
+	639,
+	'Princess',
+	'Quebec',
+	'14 nopti',
+	'De la 1121 EUR'
+);
+
+INSERT INTO Croaziera VALUES (
+	640,
+	'Sky',
+	'Capitalele Baltice',
+	'14 nopti',
+	'De la 1425 EUR'
+);
+
+INSERT INTO Categorie VALUES (
+	310,
+	'Mancare'
+);
+
+INSERT INTO Categorie VALUES (
+	311,
+	'Desert'
+);
+
+INSERT INTO Categorie VALUES (
+	312,
+	'Bautura'
+);
+
+INSERT INTO Alimente VALUES (
+	510,
+	'Fructe de mare',
+	310
+);
+
+INSERT INTO Alimente VALUES (
+	511,
+	'Paste',
+	310
+);
+
+INSERT INTO Alimente VALUES (
+	512,
+	'Salata asparagus',
+	310
+);
+INSERT INTO Alimente VALUES (
+	513,
+	'Ciuperci cu parmezan',
+	310
+);
+
+INSERT INTO Alimente VALUES (
+	514,
+	'crema alba de trufe',
+	310
+);
+
+INSERT INTO Alimente VALUES (
+	515,
+	'Paella',
+	310
+);
+
+INSERT INTO Alimente VALUES (
+	516,
+	'Saffron couscous',
+	310
+);
+
+INSERT INTO Alimente VALUES (
+	517,
+	'Trufe negre cu risotto',
+	310
+);
+
+INSERT INTO Alimente VALUES (
+	518,
+	'Friptura de miel',
+	310
+);
+
+INSERT INTO Alimente VALUES (
+	519,
+	'Souffle',
+	311
+);
+
+INSERT INTO Alimente VALUES (
+	520,
+	'Creme brulee',
+	311
+);
+
+INSERT INTO Alimente VALUES (
+	521,
+	'Antreu',
+	310
+);
+
+INSERT INTO Alimente VALUES (
+	522,
+	'Sos remoulade',
+	310
+);
+
+INSERT INTO Alimente VALUES (
+	523,
+	'Branzeturi albe',
+	310
+);
+
+INSERT INTO Alimente VALUES (
+	524,
+	'Vinuri vechi',
+	312
+);
+
+INSERT INTO Alimente VALUES (
+	525,
+	'Espresso',
+	312
+);
+
+INSERT INTO Alimente VALUES (
+	526,
+	'Cappuccino',
+	312
+);
+
+INSERT INTO Alimente VALUES (
+	527,
+	'Latte',
+	312
+);
+
+INSERT INTO Alimente VALUES (
+	528,
+	'Paste cu trufe',
+	310
+);
+
+INSERT INTO Alimente VALUES (
+	529,
+	'Pizza',
+	310
+);
+
+INSERT INTO Alimente VALUES (
+	530,
+	'Sushi',
+	310
+);
+
+INSERT INTO Alimente VALUES (
+	531,
+	'Mancare chinezeasca',
+	310
+);
+
+INSERT INTO Alimente VALUES (
+	532,
+	'Supe',
+	310
+);
+
+INSERT INTO Alimente VALUES (
+	533,
+	'Peste marinat',
+	310
+);
+
+INSERT INTO Alimente VALUES (
+	534,
+	'Caviar',
+	310
+);
+
+INSERT INTO Alimente VALUES (
+	535,
+	'Fois gras',
+	310
+);
+
+INSERT INTO Alimente VALUES (
+	536,
+	'Carpaccio din rata afumata',
+	310
+);
+
+INSERT INTO Alimente VALUES (
+	537,
+	'Prepelita pane',
+	310
+);
+
+INSERT INTO Alimente VALUES (
+	538,
+	'Sorbet din fructul pasiunii',
+	311
+);
+
+INSERT INTO Alimente VALUES (
+	539,
+	'Tartar de ton',
+	310
+);
+
+INSERT INTO Alimente VALUES (
+	540,
+	'Cocktail',
+	312
+);
+
+INSERT INTO Alimente VALUES (
+	541,
+	'Ceaiuri',
+	312
+);
+
+INSERT INTO Alimente VALUES (
+	542,
+	'Whisky',
+	312
+);
+
+INSERT INTO Alimente VALUES (
+	543,
+	'Bere',
+	312
+);
+
+INSERT INTO Alimente VALUES (
+	544,
+	'Racoritoare',
+	312
+);
+
+INSERT INTO Alimente VALUES (
+	545,
+	'Apa',
+	312
+);
+
+INSERT INTO Alimente VALUES (
+	546,
+	'Pepperoni',
+	310
+);
+
+INSERT INTO Port VALUES (
+	410,
+	'Fuerteventura',
+	'2 zile'
+);
+
+INSERT INTO Port VALUES (
+	411,
+	'Lanzarote',
+	'1 zi'
+);
+
+INSERT INTO Port VALUES (
+	412,
+	'Madeira',
+	'1 zi'
+);
+
+INSERT INTO Port VALUES (
+	413,
+	'Mykonos',
+	'1 zi'
+);
+
+INSERT INTO Port VALUES (
+	414,
+	'Corsica',
+	'3 zile'
+);
+
+INSERT INTO Port VALUES (
+	415,
+	'Palma de Mallorca',
+	'2 zile'
+);
+
+INSERT INTO Port VALUES (
+	416,
+	'Civitavecchia',
+	'3 zile'
+);
+
+INSERT INTO Port VALUES (
+	417,
+	'Fort Lauderdale',
+	'1 zi'
+);
+
+INSERT INTO Port VALUES (
+	418,
+	'Cozumel',
+	'1 zi'
+);
+
+INSERT INTO Port VALUES (
+	419,
+	'Mexic',
+	'3 zile'
+);
+
+INSERT INTO Port VALUES (
+	420,
+	'Puerto Rico',
+	'1 zi'
+);
+
+INSERT INTO Port VALUES (
+	421,
+	'Athena',
+	'4 zile'
+);
+
+INSERT INTO Port VALUES (
+	422,
+	'Olden',
+	'2 zile'
+);
+
+INSERT INTO Port VALUES (
+	423,
+	'Cannes',
+	'3 zile'
+);
+
+INSERT INTO Port VALUES (
+	424,
+	'Sitka',
+	'3 zile'
+);
+
+INSERT INTO Port VALUES (
+	425,
+	'Vancouver',
+	'5 zile'
+);
+
+INSERT INTO Port VALUES (
+	426,
+	'Norvegia-Nesna',
+	'3 zile'
+);
+
+INSERT INTO Port VALUES (
+	427,
+	'Canaveral',
+	'1 zi'
+);
+
+INSERT INTO Port VALUES (
+	428,
+	'Genova',
+	'2 zile'
+);
+
+INSERT INTO Port VALUES (
+	429,
+	'Le Havre',
+	'2 zile'
+);
+
+INSERT INTO Port VALUES (
+	430,
+	'St.Petersburg',
+	'1 zi'
+);
